a3094d8dbc1673f4a5c59de0a6603fd3e58ff190
vivian347 <vkinyua001@gmail.com>
2023-04-03 02:26:19 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+"""Determines whether all locked boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    unlocked = [0]
+    for id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != id:
+                unlocked.append(key)
+
+    if len(unlocked) == len(boxes):
+        return True
+    return False
