fd84e54ca5e65b9a9e7bff85a58f78202aa6c272
vivian347 <vkinyua001@gmail.com>
2023-05-07 02:31:40 +0300

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,24 +4,21 @@
  * prints characteres in a star wars movie as listed in the /films/ endpoint
  */
 
+const request = require('request');
 
-const request = require('request')
-
-movie_id = process.argv[2]
-url = `https://swapi.dev/api/films/${movie_id}/`
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, async (err, response, body) => {
-    if (err) return console.error(err);
-    const char_url_list = JSON.parse(body).characters;
-    for (const char_url of char_url_list) {
-        await new Promise((resolve, reject) => {
-            request(char_url, (err, response, body) => {
-                if (err) return console.error(err);
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    };
+  if (err) return console.error(err);
+  const charUrlList = JSON.parse(body).characters;
+  for (const charUrl of charUrlList) {
+    await new Promise((resolve, reject) => {
+      request(charUrl, (err, response, body) => {
+        if (err) return console.error(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
-
-
